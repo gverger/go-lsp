2cb02e13505b9e24055884aa198a593d34db8662
Guillaume Verger <verger.guillaume@gmail.com>
2022-09-27 22:37:38 +0200

    TextDocument path

--- lsp/methods_gen.go
+++ lsp/methods_gen.go
@@ -242,7 +242,7 @@ func (m *Methods) didOpenTextDocumentMethodInfo() *jsonrpc.MethodInfo {
 		return nil
 	}
 	return &jsonrpc.MethodInfo{
-		Name: "didOpenTextDocument",
+		Name: "textDocument/didOpen",
 		NewRequest: func() interface{} {
 			return &defines.DidOpenTextDocumentParams{}
 		},
@@ -270,7 +270,7 @@ func (m *Methods) didChangeTextDocumentMethodInfo() *jsonrpc.MethodInfo {
 		return nil
 	}
 	return &jsonrpc.MethodInfo{
-		Name: "didChangeTextDocument",
+		Name: "textDocument/didChange",
 		NewRequest: func() interface{} {
 			return &defines.DidChangeTextDocumentParams{}
 		},
@@ -298,7 +298,7 @@ func (m *Methods) didCloseTextDocumentMethodInfo() *jsonrpc.MethodInfo {
 		return nil
 	}
 	return &jsonrpc.MethodInfo{
-		Name: "didCloseTextDocument",
+		Name: "textDocument/didClose",
 		NewRequest: func() interface{} {
 			return &defines.DidCloseTextDocumentParams{}
 		},
@@ -326,7 +326,7 @@ func (m *Methods) willSaveTextDocumentMethodInfo() *jsonrpc.MethodInfo {
 		return nil
 	}
 	return &jsonrpc.MethodInfo{
-		Name: "willSaveTextDocument",
+		Name: "textDocument/willSave",
 		NewRequest: func() interface{} {
 			return &defines.WillSaveTextDocumentParams{}
 		},
@@ -354,7 +354,7 @@ func (m *Methods) didSaveTextDocumentMethodInfo() *jsonrpc.MethodInfo {
 		return nil
 	}
 	return &jsonrpc.MethodInfo{
-		Name: "didSaveTextDocument",
+		Name: "textDocument/didSave",
 		NewRequest: func() interface{} {
 			return &defines.DidSaveTextDocumentParams{}
 		},
